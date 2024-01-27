b30912bfe98a8b101726d06342044268e1a61c70
musab.bozkurt <musab.bozkurt@ozan.com>
2024-01-28 01:24:23 +0300

    spring-cloud-gateway with springDoc migration is completed under api-gateway - cleanup

--- api-gateway/src/main/java/com/mb/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mb/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,9 @@
 package com.mb.apigateway;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "API Gateway with Spring Cloud Gateway"))
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
