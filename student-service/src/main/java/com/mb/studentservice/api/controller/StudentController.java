51bfea25f7bf6f578bd07e498cd310563552fac6
musab.bozkurt <musab.bozkurt@ozan.com>
2024-01-27 17:25:42 +0300

    student-service - spring boot 3, java 21 and swagger upgrade

--- student-service/src/main/java/com/mb/studentservice/api/controller/StudentController.java
+++ student-service/src/main/java/com/mb/studentservice/api/controller/StudentController.java
@@ -5,11 +5,8 @@ import com.mb.studentservice.client.payment.response.PaymentResponse;
 import com.mb.studentservice.enums.EventType;
 import com.mb.studentservice.queue.producer.StudentEventProducer;
 import com.mb.studentservice.service.StudentService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +18,6 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/students")
-@Api(value = "Student Rest Controller")
 public class StudentController {
 
     private final StudentEventProducer studentEventProducer;
@@ -36,42 +32,32 @@ public class StudentController {
         students.add(new Student(4, "Student-Student4", "USER3", "student.student4@test.com"));
     }
 
-    @RequestMapping(value = "/")
-    @ApiOperation(value = "Get Students", response = Iterable.class, tags = "getStudents")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success|OK"),
-            @ApiResponse(code = 401, message = "Not Authorized!"),
-            @ApiResponse(code = 403, message = "Forbidden!"),
-            @ApiResponse(code = 404, message = "Not Found!")})
+    @GetMapping(value = "/")
     public List<Student> getStudents() {
         return students;
     }
 
-    @RequestMapping(value = "/{id}")
-    @ApiOperation(value = "Get Student by Student Id ", response = Student.class, tags = "getStudentById")
+    @GetMapping(value = "/{id}")
     public Student getStudentById(@PathVariable(value = "id") int id) {
         return students.stream()
                 .filter(x -> x.getId() == (id))
-                .collect(Collectors.toList())
-                .get(0);
+                .toList()
+                .getFirst();
     }
 
-    @RequestMapping(value = "/role/{role}")
-    @ApiOperation(value = "Get Student by role ", response = Student.class, tags = "getStudentByRole")
+    @GetMapping(value = "/roles/{role}")
     public List<Student> getStudentByRole(@PathVariable(value = "role") String role) {
         return students.stream()
                 .filter(x -> x.getRole().equalsIgnoreCase(role))
                 .collect(Collectors.toList());
     }
 
-    @RequestMapping(value = "/events")
-    @ApiOperation(value = "Publish Student Event", response = Student.class, tags = "publishStudentEvent")
+    @GetMapping(value = "/events")
     public void publishStudentEvent() {
         studentEventProducer.publishEvent("Publish Student Event with EventType STUDENT_EVENT", EventType.STUDENT_EVENT);
     }
 
-    @RequestMapping(value = "/payments")
-    @ApiOperation(value = "Get Payments", response = PaymentResponse.class, tags = "getPayments")
+    @GetMapping(value = "/payments")
     public List<PaymentResponse> getPayments() {
         return studentService.getPayments();
     }
