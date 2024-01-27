cc6c7476903f16ee847d033e5caa7642dc448c16
musab.bozkurt <musab.bozkurt@ozan.com>
2024-01-27 17:28:03 +0300

    student-service - rename swagger2

--- student-service/src/main/java/com/mb/studentservice/enums/EventType.java
+++ student-service/src/main/java/com/mb/studentservice/enums/EventType.java
@@ -1,6 +1,6 @@
 package com.mb.studentservice.enums;
 
 public enum EventType {
-    SWAGGER2_EVENT,
+    SWAGGER_EVENT,
     STUDENT_EVENT
 }
--- student-service/src/main/java/com/mb/studentservice/queue/consumer/SwaggerEventConsumer.java
+++ student-service/src/main/java/com/mb/studentservice/queue/consumer/SwaggerEventConsumer.java
@@ -12,20 +12,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Component
 @AllArgsConstructor
-public class Swagger2EventConsumer implements EventConsumerStrategy {
+public class SwaggerEventConsumer implements EventConsumerStrategy {
 
     @Override
     @Transactional(noRollbackFor = RuntimeException.class) // If the system is transactional then it can be used.
     public void execute(Message<?> message) {
         try {
-            log.info("Message is consumed in Swagger2EventConsumer. Message : {}", message);
+            log.info("Message is consumed in SwaggerEventConsumer. Message : {}", message);
         } catch (Exception e) {
-            log.error("Exception occurred while executing Swagger2EventConsumer. Exception : {}", ExceptionUtils.getStackTrace(e));
+            log.error("Exception occurred while executing SwaggerEventConsumer. Exception : {}", ExceptionUtils.getStackTrace(e));
         }
     }
 
     @Override
     public boolean canExecute(String eventType) {
-        return EventType.SWAGGER2_EVENT.name().equals(eventType);
+        return EventType.SWAGGER_EVENT.name().equals(eventType);
     }
 }
