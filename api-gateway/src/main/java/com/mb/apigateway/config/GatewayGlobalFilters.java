2a215c29a652ace0f879b967501b4a4bcbbf534a
musab.bozkurt <musab.bozkurt@ozan.com>
2024-02-08 00:22:59 +0300

    XSSRequestWrapper is added and more investigation should be done

--- api-gateway/src/main/java/com/mb/apigateway/config/GatewayGlobalFilters.java
+++ api-gateway/src/main/java/com/mb/apigateway/config/GatewayGlobalFilters.java
@@ -1,5 +1,9 @@
 package com.mb.apigateway.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +11,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import reactor.core.publisher.Mono;
 
+import java.io.InputStream;
+import java.util.Objects;
+
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class GatewayGlobalFilters {
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     @Order(-1)
@@ -34,7 +44,20 @@ public class GatewayGlobalFilters {
     public GlobalFilter c() {
         return (exchange, chain) -> {
             log.info("third pre filter. Path: {}", exchange.getRequest().getURI().getPath());
+            new XSSRequestWrapper((HttpServletRequest) exchange.getRequest());
             return chain.filter(exchange).then(Mono.fromRunnable(() -> log.info("first post filter")));
         };
+    }
+
+    // TODO more XSSRequestWrapper investigation should be done
+    private String sanitiseResponse(InputStream responseDataStream) {
+        if (Objects.nonNull(responseDataStream)) {
+            try {
+                return XSSRequestWrapper.cleanXss(objectMapper.writeValueAsString(responseDataStream));
+            } catch (JsonProcessingException e) {
+                log.error("Unable to sanitize response: {}", responseDataStream);
+            }
+        }
+        return null;
     }
 }
