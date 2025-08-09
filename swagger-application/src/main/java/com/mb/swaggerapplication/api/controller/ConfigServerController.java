0370b2a976320fb62cf3ac7eb63461f5cf3a52c0
musabbozkurt <b_musab@hotmail.com>
2025-08-09 10:23:28 +0300

    Merge pull request #19
    * add-spring-cloud-config-server

---
+++ swagger-application/src/main/java/com/mb/swaggerapplication/api/controller/ConfigServerController.java
@@ -0,0 +1,22 @@
+package com.mb.swaggerapplication.api.controller;
+
+import com.mb.swaggerapplication.config.ConfigServerProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+class ConfigServerController {
+
+    private final ConfigServerProperties configServerProperties;
+
+    @Value("${service.name:null}")
+    private String serviceName;
+
+    @GetMapping("/service-name")
+    public String getServiceName() {
+        return serviceName + " - " + configServerProperties.getName();
+    }
+}
